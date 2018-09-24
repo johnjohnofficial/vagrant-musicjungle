a6498fe740cd112e597768ac7a5a7d870084c43a
JohnJohn <jferreira.ba@gmail.com>
2018-09-24 03:37:35 -0300

    finish

--- manifests/bootstrap.sh
+++ manifests/bootstrap.sh
@@ -1,4 +1,17 @@
-#!/bin/bash
+#!/bin/sh
+set -e -x
 
-sudo apt-get update
-sudo apt-get install puppet -y
+if which puppet > /dev/null ; then
+    echo "Puppet is already installed"
+    exit 0
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+wget -qO /tmp/puppetlabs-release-precise.deb https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+
+dpkg -i /tmp/puppetlabs-release-precise.deb
+rm /tmp/puppetlabs-release-precise.deb
+aptitude update
+echo Installing puppet
+aptitude install -y puppet
+echo "Puppet installed!"
